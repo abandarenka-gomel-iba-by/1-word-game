fb64b39eae0b646c0cb4a10966b4e1d08d829a71
Anton <bondarenko4488@gmail.com>
2024-06-30 22:12:17 +0200

    Pilot #2 corrected

---
+++ 1-word-game/InputOutput.cs
@@ -0,0 +1,39 @@
+﻿namespace _1_word_game
+{
+    internal class InputOutput
+    {
+        static public void PrintMessages(string[] messages, bool clearScreen = false)
+        {
+            if (clearScreen) Console.Clear();
+
+            for (int i = 0; i < messages.Length; i++)
+            {
+                Console.WriteLine(messages[i]);
+            }
+        }
+
+        static public string GetInput(string message)
+        {
+            Console.Write(message);
+            string? input = Console.ReadLine();
+            if (input == null) return "";
+            return input;
+        }
+
+        static public void PrintUnderCursorAndGoBack(string[] messages, int lineShift = 1, bool cleanOldLine = false)
+        {
+            int currentLineCursor = Console.CursorTop;
+            int currenPosCursor = Console.CursorLeft;
+            Console.SetCursorPosition(0, currentLineCursor + lineShift);
+
+            if (cleanOldLine)
+            {
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(0, currentLineCursor + lineShift);
+            }
+
+            PrintMessages(messages);
+            Console.SetCursorPosition(currenPosCursor, currentLineCursor);
+        }
+    }
+}
---
+++ 1-word-game/Validator.cs
@@ -0,0 +1,97 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _1_word_game
+{
+    internal class Validator
+    {
+        private string _input = string.Empty;
+        private string _inputError = string.Empty;
+        public Validator() { }
+        public void UpdateInput(string input)
+        {
+            _input = input;
+        }
+
+        public void ValidationInputOriginalWord()
+        {
+            if (string.IsNullOrEmpty(_input))
+            {
+                _inputError = "Input is empty! Please try again";
+                return;
+            }
+            if (_input.Length < 8)
+            {
+                _inputError = "The word is too short! Please try again.";
+                return;
+            }
+            if (_input.Length > 30)
+            {
+                _inputError = "The word is too long! Please try again.";
+                return;
+            }
+            if (Regex.IsMatch(_input, @"[^a-zA-Z]"))
+            {
+                _inputError = "The word should contain only letters! Please try again.";
+                return;
+            }
+            _inputError = string.Empty;
+        }
+
+        public void ValidationPlayerWord(string originalWord, string[] allWords)
+        {
+            if (string.IsNullOrEmpty(_input))
+            {
+                _inputError = "Empty input! Plaese try again.";
+                return;
+            }
+
+            string testWord = _input.ToLower();
+
+            foreach (string w in allWords)
+            {
+                if (testWord == w)
+                {
+                    _inputError = $"The word '{_input}' is already exist! Please try again.";
+                    return;
+                }
+            }
+
+            foreach (char c in originalWord.ToLower())
+            {
+                int index = testWord.IndexOf(c);
+
+                if (index >= 0)
+                {
+                    testWord = testWord.Remove(index, 1);
+                }
+            }
+            if (!string.IsNullOrEmpty(testWord))
+            {
+                _inputError = $"The word '{_input}' cannot be create from original word! Please try again.";
+                return;
+            }
+
+            _inputError = string.Empty;
+        }
+
+        public string GetInput()
+        {
+            return _input;
+        }
+
+        public bool IsError()
+        { 
+            return !string.IsNullOrEmpty(_inputError);
+        }
+
+        public string InputError()
+        {
+            return _inputError;
+        }
+
+        public bool IsInputCommand()
+        {
+            return _input.StartsWith("/");
+        }
+    }
+}
